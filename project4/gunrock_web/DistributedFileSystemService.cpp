82ba70f810541985044b2b2eb0af2e679d11dbb2
jotyot <104177975+jotyot@users.noreply.github.com>
2024-06-08 15:10:07 -0700

    ok i should try docker but idk how

--- project4/gunrock_web/DistributedFileSystemService.cpp
+++ project4/gunrock_web/DistributedFileSystemService.cpp
@@ -21,51 +21,54 @@ DistributedFileSystemService::DistributedFileSystemService(string diskFile) : Ht
 }  
 
 void DistributedFileSystemService::get(HTTPRequest *request, HTTPResponse *response) {
-  vector<string> pathComponents = request->getPathComponents();
-  if (pathComponents.size() < 1 || pathComponents[0] != "ds3") {
-    throw ClientError::badRequest();
-  }
+  response->setBody("");
+  // vector<string> pathComponents = request->getPathComponents();
+  // if (pathComponents.size() < 1 || pathComponents[0] != "ds3") {
+  //   throw ClientError::badRequest();
+  // }
 
-  int currentInum = UFS_ROOT_DIRECTORY_INODE_NUMBER;
+  // int currentInum = UFS_ROOT_DIRECTORY_INODE_NUMBER;
 
-  for (unsigned int i = 1; i < pathComponents.size(); i++) {
-    int nextInum = fileSystem->lookup(currentInum, pathComponents[i]);
-    if (nextInum < 0) {
-      throw ClientError::notFound();
-    }
-    currentInum = nextInum;
-  }
+  // for (unsigned int i = 1; i < pathComponents.size(); i++) {
+  //   int nextInum = fileSystem->lookup(currentInum, pathComponents[i]);
+  //   if (nextInum < 0) {
+  //     throw ClientError::notFound();
+  //   }
+  //   currentInum = nextInum;
+  // }
 
-  inode_t inode;
-  fileSystem->stat(currentInum, &inode);
+  // inode_t inode;
+  // fileSystem->stat(currentInum, &inode);
 
-  if (inode.type == UFS_DIRECTORY) {
-    string output;
-    dir_ent_t entries[inode.size / sizeof(dir_ent_t)];
-    fileSystem->read(currentInum, entries, inode.size);
+  // if (inode.type == UFS_DIRECTORY) {
+  //   string output;
+  //   dir_ent_t entries[inode.size / sizeof(dir_ent_t)];
+  //   fileSystem->read(currentInum, entries, inode.size);
 
-    sort(entries + 2, entries + (inode.size / sizeof(dir_ent_t)), [](dir_ent_t a, dir_ent_t b) {
-        return strcmp(a.name, b.name) < 0;
-    });
+  //   sort(entries + 2, entries + (inode.size / sizeof(dir_ent_t)), [](dir_ent_t a, dir_ent_t b) {
+  //       return strcmp(a.name, b.name) < 0;
+  //   });
 
-    for (int i = 2; i < inode.size / (int)sizeof(dir_ent_t); i++) {
-      inode_t entryInode;
-      fileSystem->stat(entries[i].inum, &entryInode);
-      output += entries[i].name;
-      if (entryInode.type == UFS_DIRECTORY) {
-        output += "/";
-      }
-      output += "\n";
-    }
-    response->setBody(output);
-  } else {
-    char buffer[inode.size];
-    fileSystem->read(currentInum, buffer, inode.size);
-    response->setBody(string(buffer, inode.size));
-  }
+  //   for (int i = 2; i < inode.size / (int)sizeof(dir_ent_t); i++) {
+  //     inode_t entryInode;
+  //     fileSystem->stat(entries[i].inum, &entryInode);
+  //     output += entries[i].name;
+  //     if (entryInode.type == UFS_DIRECTORY) {
+  //       output += "/";
+  //     }
+  //     output += "\n";
+  //   }
+  //   response->setBody(output);
+  // } else {
+  //   char buffer[inode.size];
+  //   fileSystem->read(currentInum, buffer, inode.size);
+  //   response->setBody(string(buffer, inode.size));
+  // }
 }
   
 void DistributedFileSystemService::put(HTTPRequest *request, HTTPResponse *response) {
+  response->setBody("");
+
   vector<string> pathComponents = request->getPathComponents();
   if (pathComponents.size() < 2 || pathComponents[0] != "ds3") {
     throw ClientError::badRequest();
@@ -99,39 +102,6 @@ void DistributedFileSystemService::put(HTTPRequest *request, HTTPResponse *respo
   string contents = request->getBody();
 
   int ret = fileSystem->write(currentInum, contents.c_str(), contents.size());
-  if (ret == -ENOTENOUGHSPACE) {
-    fileSystem->disk->rollback();
-    throw ClientError::insufficientStorage();
-  }
-
-  fileSystem->disk->commit();
-
-  response->setBody("");
-}
-
-void DistributedFileSystemService::del(HTTPRequest *request, HTTPResponse *response) {
-  vector<string> pathComponents = request->getPathComponents();
-
-  if (pathComponents.size() < 2 || pathComponents[0] != "ds3") {
-    throw ClientError::badRequest();
-  }
-
-  int currentInum = UFS_ROOT_DIRECTORY_INODE_NUMBER;
-
-  for (unsigned int i = 1; i < pathComponents.size() - 1; i++) {
-    int nextInum = fileSystem->lookup(currentInum, pathComponents[i]);
-    if (nextInum < 0) {
-      throw ClientError::notFound();
-    }
-    currentInum = nextInum;
-  }
-
-  fileSystem->disk->beginTransaction();
-
-  inode_t inode;
-  fileSystem->stat(currentInum, &inode);
-
-  int ret = fileSystem->unlink(currentInum, pathComponents[pathComponents.size() - 1]);
   if (ret < 0) {
     fileSystem->disk->rollback();
     if (ret == -ENOTENOUGHSPACE) {
@@ -140,8 +110,43 @@ void DistributedFileSystemService::del(HTTPRequest *request, HTTPResponse *respo
       throw ClientError::badRequest();
     }
   }
-  
+
   fileSystem->disk->commit();
+}
+
+void DistributedFileSystemService::del(HTTPRequest *request, HTTPResponse *response) {
+  // vector<string> pathComponents = request->getPathComponents();
+
+  // if (pathComponents.size() < 2 || pathComponents[0] != "ds3") {
+  //   throw ClientError::badRequest();
+  // }
+
+  // int currentInum = UFS_ROOT_DIRECTORY_INODE_NUMBER;
+
+  // for (unsigned int i = 1; i < pathComponents.size() - 1; i++) {
+  //   int nextInum = fileSystem->lookup(currentInum, pathComponents[i]);
+  //   if (nextInum < 0) {
+  //     throw ClientError::notFound();
+  //   }
+  //   currentInum = nextInum;
+  // }
+
+  // fileSystem->disk->beginTransaction();
+
+  // inode_t inode;
+  // fileSystem->stat(currentInum, &inode);
+
+  // int ret = fileSystem->unlink(currentInum, pathComponents[pathComponents.size() - 1]);
+  // if (ret < 0) {
+  //   fileSystem->disk->rollback();
+  //   if (ret == -ENOTENOUGHSPACE) {
+  //     throw ClientError::insufficientStorage();
+  //   } else {
+  //     throw ClientError::badRequest();
+  //   }
+  // }
+  
+  // fileSystem->disk->commit();
 
   response->setBody("");
 }
